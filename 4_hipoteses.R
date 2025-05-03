862584dd1963540b92cbadd23194e4ce2b824c86
Giovanna de Oliveira Brogliato <85835788+gibrogliato@users.noreply.github.com>
2025-05-03 20:55:54 -0300

    Testes de hipóteses (usando ANOVA)

--- 4_hipoteses.R
+++ 4_hipoteses.R
@@ -1,5 +1,5 @@
 # Descrição do Script:
-# Esse script realiza testes de hipóteses (usando ANOVA) para verificar se existem diferenças significativas
+# Esse script realiza testes de hipótese (usando ANOVA) para verificar se existem diferenças significativas
 # nas médias de diversas métricas de saúde (doenças cardiovasculares, diabetes, colesterol, etc) 
 # entre pacientes com e sem diagnóstico de Alzheimer.
 
@@ -18,15 +18,23 @@
 # 1. '1_analise_descritiva.R'
 # 2. '2_plot_histogramas.R'
 # 3. '3_correlatos.R'
-# 4. '4_novas_hipoteses'
+# 4. '4_hipoteses'
 # '---------------------------------------------------------'
 
 #Agora entendendo que algumas variáveis podem ter correlações, vamos propor hipóteses.
+
 #Verificando componentes do teste
 # Teste F para comparar as variâncias entre duas variáveis contínuas
 #Para esses testes vamos assumir a H0 de que as variâncias são iguais e H1 de que não são iguais
 #Variáveis: "CardiovascularDisease", "Diabetes", "Depression", "Hypertension",
-#"CholesterolTotal", "CholesterolLDL", "CholesterolHDL", "CholesterolTriglycerides"
+#"CholesterolTotal", "CholesterolLDL", "CholesterolHDL", "CholesterolTriglycerides",
+# "Age","BMI", "PhysicalActivity","SleepQuality", "AlcoholConsumption"
+
+var.test(Age ~ Diagnosis, data = dados_alzheimer)
+var.test(BMI ~ Diagnosis, data = dados_alzheimer)
+var.test(PhysicalActivity ~ Diagnosis, data = dados_alzheimer)
+var.test(SleepQuality ~ Diagnosis, data = dados_alzheimer)
+var.test(AlcoholConsumption ~ Diagnosis, data = dados_alzheimer)
 
 var.test(CardiovascularDisease ~ Diagnosis, data = dados_alzheimer)
 var.test(Diabetes ~ Diagnosis, data = dados_alzheimer)
@@ -36,6 +44,8 @@ var.test(CholesterolTotal ~ Diagnosis, data = dados_alzheimer)
 var.test(CholesterolLDL ~ Diagnosis, data = dados_alzheimer)
 var.test(CholesterolHDL ~ Diagnosis, data = dados_alzheimer)
 var.test(CholesterolTriglycerides ~ Diagnosis, data = dados_alzheimer)
+
+
 
 #Podemos proceder para estudos por ANOVA para as variáveis.
 
@@ -110,8 +120,55 @@ summary(anova_result_hdl)
 anova_result_trigl <- aov(CholesterolTriglycerides ~ Diagnosis, data = dados_alzheimer)
 summary(anova_result_trigl)
 
+#Idade e Diagnóstico
+#Hipótese Nula (H0): Não há diferença na média de idade entre os pacientes com diagnóstico positivo e negativo.
+#Hipótese Alternativa (H1): Existe uma diferença significativa na média de idade entre os pacientes com diagnóstico positivo e negativo.
+
+anova_result_id <- aov(Age ~ Diagnosis, data = dados_alzheimer)
+summary(anova_result_id)
+
+#Não há diferença significativa entre os grupos e eu posso aceitar a hipótese nula.
+
+#IMC e Diagnóstico
+#Hipótese Nula (H0): Não há diferença significativa no IMC entre pacientes com diagnóstico positivo e negativo.
+#Hipótese Alternativa (H1): Existe uma diferença significativa no IMC entre pacientes com diagnóstico positivo e negativo.
+
+anova_result_bmi <- aov(BMI ~ Diagnosis, data = dados_alzheimer)
+summary(anova_result_bmi)
+
+#Não há diferença significativa entre os grupos e eu posso aceitar a hipótese nula.
+
+#Qualidade de Sono e Diagnóstico
+#Hipótese Nula (H0): Não há diferença na qualidade do sono entre os pacientes com diagnóstico positivo e negativo.
+#Hipótese Alternativa (H1): Existe uma diferença significativa na qualidade do sono entre os pacientes com diagnóstico positivo e negativo.
+
+
+anova_result_sono <- aov(SleepQuality ~ Diagnosis, data = dados_alzheimer)
+summary(anova_result_sono)
+
+#Há diferença significativa entre os grupos e eu posso rejeitar a hipótese nula.
+
+
+#Consumo de Álcool e Diagnóstico
+#Hipótese Nula (H0): Não há diferença no consumo de álcool entre os pacientes com diagnóstico positivo e negativo.
+#Hipótese Alternativa (H1): Existe uma diferença significativa no consumo de álcool entre os pacientes com diagnóstico positivo e negativo.
+
+
+anova_result_alcool <- aov(AlcoholConsumption ~ Diagnosis, data = dados_alzheimer)
+summary(anova_result_alcool)
+
+#Não há diferença significativa entre os grupos e eu posso aceitar a hipótese nula.
+
+#Atividade Física e Diagnóstico
+#Hipótese Nula (H0): Não há diferença na atividade física entre os pacientes com diagnóstico positivo e negativo.
+#Hipótese Alternativa (H1): Existe uma diferença significativa na atividade física entre os pacientes com diagnóstico positivo e negativo.
+
+anova_result_af <- aov(PhysicalActivity ~ Diagnosis, data = dados_alzheimer)
+summary(anova_result_af)
+
 # Visualizando os resultados da ANOVA
-variaveis <- c("CardiovascularDisease", "Diabetes", "Depression", "Hypertension",
+variaveis <- c("Age","BMI", "PhysicalActivity","SleepQuality", "AlcoholConsumption",
+               "CardiovascularDisease", "Diabetes", "Depression", "Hypertension",
                "CholesterolTotal", "CholesterolLDL", "CholesterolHDL", "CholesterolTriglycerides")
 
 # Criar uma tabela com os valores-p da ANOVA
@@ -146,6 +203,25 @@ boxplot(CholesterolHDL ~ Diagnosis, data = dados_alzheimer, col = "lightblue")
 #Isso indica que pacientes com Alzheimer (grupo 1) têm, em média, 2.06 unidades a mais de HDL que os pacientes sem Alzheimer.
 
 
+#Como Na Qualidade de Sono também temos uma diferença significativa dos dados, quero saber onde ela está. 
+#Por isso farei um teste HSD Tukey
+
+# Realizando o teste de Tukey para identificar quais grupos são diferentes
+# Convertendo a variável Diagnosis para fator, para o Tukey funcionar
+dados_alzheimer$Diagnosis <- as.factor(dados_alzheimer$Diagnosis)
+anova_result_sono <- aov(SleepQuality ~ Diagnosis, data = dados_alzheimer)
+TukeyHSD(anova_result_sono)
+
+#Visualizando
+boxplot(SleepQuality ~ Diagnosis, data = dados_alzheimer, col = "lightblue")
+
+#A análise post-hoc de Tukey revelou que os indivíduos com diagnóstico positivo de Alzheimer apresentaram qualidade de sono significativamente menor 
+#do que os sem diagnóstico (p = 0.01),enquanto não houve diferença entre os demais grupos.
+
+
 #Conclusão
-#A partir da análise de variância realizada, apenas o colesterol HDL apresentou diferença estatisticamente significativa entre os grupos com e sem diagnóstico de Alzheimer.
+#A partir da análise de variância realizada, o colesterol HDL apresentou diferença estatisticamente significativa entre os grupos com e sem diagnóstico de Alzheimer.
 #Isso sugere que o HDL pode estar associado ao diagnóstico na amostra analisada.
+
+#SleepQuality foi uma ariável preditora significativa com p < 0.05. Isso significa que, mantidas as outras variáveis constantes, 
+#pessoas com qualidade de sono mais baixa têm maior chance de diagnóstico positivo para Alzheimer.
